ca647af0cb4709f2801bceb9a20a2339f3c9d378
Antoine Buchser <antoine.buchser@epitech.eu>
2017-02-20 17:16:40 -0800

    Fix to activities migration

--- api/database/migrations/2017_02_16_014453_create_table_activities.php
+++ api/database/migrations/2017_02_16_014453_create_table_activities.php
@@ -27,7 +27,7 @@ class CreateTableActivities extends Migration
         });
 		
 		Schema::table('activities', function (Blueprint $table) {
-            $table->foreign('creator_id')->references('id')->on('users')->onDelete('set null');
+            $table->foreign('creator_id')->references('id')->on('users')->onDelete('cascade');
 			$table->foreign('community_id')->references('id')->on('communities')->onDelete('set null');
 			$table->foreign('category_id')->references('id')->on('categories')->onDelete('set null');
         });
