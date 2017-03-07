156b4a053d5e8361e0dde3a0a4961781a28fe9ba
Caleb Evans <caleb@calebevans.me>
2017-03-06 17:57:02 -0800

    Start frontend rewrite using Mithril
    The new demo site features a simpler and cleaner UI, and includes a demo
    Sign In page.

---
+++ web/js/app.js
@@ -0,0 +1,24 @@
+(function() {
+
+var AppHeaderComponent = {};
+
+AppHeaderComponent.view = function () {
+  return [
+    m('header.app-header', [
+      m('h1.app-title', 'I\'ll B There')
+    ])
+  ]
+};
+
+var AppComponent = {};
+
+AppComponent.view = function () {
+  return [
+    m(AppHeaderComponent),
+    m(SignInComponent)
+  ];
+};
+
+m.mount(document.body, AppComponent);
+
+}());
---
+++ web/js/sign-in.js
@@ -0,0 +1,24 @@
+(function() {
+
+var SignInComponent = {};
+
+SignInComponent.view = function () {
+  return m('form[action="?"].panel.panel-sign-in', [
+    m('h2', 'Sign In'),
+    m('div.row', [
+      m('label', 'Email'),
+      m('input[type=text][autofocus].user-email')
+    ]),
+    m('div.row', [
+      m('label', 'Password'),
+      m('input[type=password].user-password')
+    ]),
+    m('div.row', [
+      m('button[type=submit].sign-in-submit', 'Sign In'),
+      m('button[type=button].sign-in-register', 'Register')
+    ])
+  ]);
+};
+
+window.SignInComponent = SignInComponent;
+}());
