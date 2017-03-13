41894284131aa820c7fc999f1647e66330c140d4
Caleb Evans <caleb@calebevans.me>
2017-03-13 09:36:31 -0700

    Mark sign-in fields as required

--- web/app/scripts/sign-in.js
+++ web/app/scripts/sign-in.js
@@ -30,11 +30,11 @@ SignInComponent.view = function (vnode) {
       m('h2', 'Sign In'),
       m('div.row', [
         m('label', 'Email'),
-        m('input[type=text][autofocus].user-email')
+        m('input[type=text][required][autofocus].user-email')
       ]),
       m('div.row', [
         m('label', 'Password'),
-        m('input[type=password].user-password')
+        m('input[type=password][required].user-password')
       ]),
       m('div.row', [
         m('button[type=submit].sign-in-submit', 'Sign In'),
