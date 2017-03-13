a9880a9de7aaefd29ac171e86d52c624e307ea3f
Caleb Evans <caleb@calebevans.me>
2017-03-13 14:28:24 -0700

    Add note about www/ directory name and Cordova

--- web/brunch-config.js
+++ web/brunch-config.js
@@ -4,7 +4,9 @@
 
 module.exports = {
   paths: {
-    // The directory the front end site is built to
+    // The directory the front end site is built to (the name 'www' is required
+    // for the Cordova mobile app framework, which may eventually be integrated
+    // into the app, so use 'www' as a name that's future-proof)
     public: 'www'
   },
   files: {
