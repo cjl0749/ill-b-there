b7b96af709a4ff4815763f237b74a70598db1425
Caleb Evans <caleb@calebevans.me>
2017-03-12 20:17:35 -0700

    Make UI panels fit to their contents
    This eliminates the need to set an explicit width/height on each panel.
    In addition, the loading indicator has been updated to overlay the
    contents of its container (which reduces DOM manipulation on Mithril's
    part, while also enabling additional flexibility in the UI design).

--- web/app/scripts/loading.js
+++ web/app/scripts/loading.js
@@ -5,8 +5,8 @@ var m = require('mithril');
 var LoadingComponent = {};
 
 LoadingComponent.view = function () {
-  return m('svg.loading', {viewBox: '0 0 320 320'}, [
-    m('path', {d: 'M 40,160 A 80,80 0,0,0 280,160'})
+  return m('div.loading', [
+    m('img', {src: 'images/loading.svg', alt: 'Loading...'})
   ]);
 };
 
--- web/app/scripts/sign-in.js
+++ web/app/scripts/sign-in.js
@@ -20,24 +20,24 @@ SignInComponent.oninit = function (vnode) {
 
 SignInComponent.view = function (vnode) {
   var state = vnode.state;
-  return m('div.panel.panel-sign-in',
-    vnode.attrs.app.authenticating ?
-      m(LoadingComponent) :
-      m('form', {method: 'POST', onsubmit: state.signIn}, [
-        m('h2', 'Sign In'),
-        m('div.row', [
-          m('label', 'Email'),
-          m('input[type=text][autofocus].user-email')
-        ]),
-        m('div.row', [
-          m('label', 'Password'),
-          m('input[type=password].user-password')
-        ]),
-        m('div.row', [
-          m('button[type=submit].sign-in-submit', 'Sign In'),
-          m('button[type=button].sign-in-register', 'Register')
-        ])
-      ]));
+  return m('div.panel.panel-sign-in', [
+    vnode.attrs.app.authenticating ? m(LoadingComponent) : null,
+    m('form', {method: 'POST', onsubmit: state.signIn}, [
+      m('h2', 'Sign In'),
+      m('div.row', [
+        m('label', 'Email'),
+        m('input[type=text][autofocus].user-email')
+      ]),
+      m('div.row', [
+        m('label', 'Password'),
+        m('input[type=password].user-password')
+      ]),
+      m('div.row', [
+        m('button[type=submit].sign-in-submit', 'Sign In'),
+        m('button[type=button].sign-in-register', 'Register')
+      ])
+    ])
+  ]);
 };
 
 module.exports = SignInComponent;
